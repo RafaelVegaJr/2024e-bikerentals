c9bc24481e1112cc85774f949288e68bc27f6f4a
Tito Vega <titovega12@gmail.com>
2025-04-22 10:26:56 -0700

    Cleaned up index.html and rebuilt project

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import "./AboutSection.css"; // Import the CSS for this component
+import "./AboutSection.css";
 
 const AboutSection = () => {
   useEffect(() => {
     const handleScroll = () => {
       const aboutTitle = document.querySelector(".about-title");
-      const aboutText = document.querySelector(".about-section pre");
+      const aboutText = document.querySelector(".about-paragraphs");
       const scrollPosition = window.scrollY;
 
       if (scrollPosition > 50) {
@@ -18,10 +18,7 @@ const AboutSection = () => {
     };
 
     window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
@@ -29,31 +26,26 @@ const AboutSection = () => {
       <div className="about-text-container">
         <h1 className="about-title">
           <span>Discover</span>
-          <br />
           <span>Our</span>
-          <br />
           <span>E-Bike</span>
-          <br />
           <span>Rentals</span>
-          <br />
         </h1>
-        <pre>
-          {`
-     Looking for an adventure?
-     We’re proud to offer the Aventon Soltera 
-     a sleek, high-performance electric bike perfect 
-     for cruising city streets or enjoying a scenic ride. 
-
-     Whether you're commuting, exploring,
-     or just out for some fresh air, our e-bike delivers 
-     the perfect mix of comfort, power, and style.
-
-     We're all about providing an easy, enjoyable 
-     rental experience with a focus on quality, 
-     simplicity, and personalized service.
-     Try it for yourself and feel the ride.
-          `}
-        </pre>
+        <div className="about-paragraphs">
+          <p>
+            Looking for an adventure? We’re proud to offer the Aventon Soltera—a
+            sleek, high-performance electric bike perfect for cruising city
+            streets or enjoying a scenic ride.
+          </p>
+          <p>
+            Whether you're commuting, exploring, or just out for some fresh air,
+            our e-bike delivers the perfect mix of comfort, power, and style.
+          </p>
+          <p>
+            We're all about providing an easy, enjoyable rental experience with
+            a focus on quality, simplicity, and personalized service. Try it for
+            yourself and feel the ride.
+          </p>
+        </div>
       </div>
     </div>
   );
